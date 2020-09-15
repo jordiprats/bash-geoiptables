b474767645bc9a1b1c23778a50e98e5b2e0ec76a
Jordi Prats <jordi.prats@gmail.com>
2020-09-15 12:33:57 +0200

    geoban

---
+++ geoban.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+
+BASEDIRSCRIPT=$(dirname $0)
+BASENAMESCRIPT=$(basename $0)
+
+IPDENY_BASEURL="https://www.ipdeny.com/ipblocks/data/countries/"
+
+GEOCHAIN="bannedcountries"
+
+if [ ! -z "$1" ] && [ -f "$1" ];
+then
+  . $1 2>/dev/null
+else
+  if [[ -s "$BASEDIRSCRIPT/${BASENAMESCRIPT%%.*}.config" ]];
+  then
+    . $BASEDIRSCRIPT/${BASENAMESCRIPT%%.*}.config 2>/dev/null
+  else
+    echo "config file missing"
+    exit 1
+  fi
+fi
+
+if [ -z "${BANED_COUNTRIES}" ];
+then
+    echo "no countries defined"
+fi
+
+iptables -X $GEOCHAIN > /dev/null 2>&1
+
+for COUNTRY in $BANED_COUNTRIES;
+do
+    OUT=$(mktemp /tmp/output.XXXXXXXXXX)
+    curl "${IPDENY_BASEURL}/${COUNTRY}.zone" -o $OUT
+
+    if [ -z "$(head -n1 $OUT)"];
+    then
+        echo "skipping country ${COUNTRY}"
+    else
+        IPS=$(egrep -v "^#|^$" $OUT)
+        for IP in $IPS
+        do
+            iptables -A $GEOCHAIN -s $IP -j LOG --log-prefix "ban country ${COUNTRY}"
+            iptables -A $GEOCHAIN -s $IP -j DROP
+        done
+    fi
+done
